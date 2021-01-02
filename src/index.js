4cb8d3575c42c4f36afb1bbd962af2ded3149bf8
shwetakalunge <shwetak@WinjitTechnologiesPvtLtd.local>
2021-01-02 14:45:21 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import add from "./Calc";
-import App from "./App.jsx";
+import Card from './Cards';
 
+import './index.css'
 ReactDOM.render(
-  <App />,
+  <>
+    <Card
+      imgsrc="https://picsum.photos/id/1/200/300"
+      title="GOT"
+      sname="got"
+      link="https://dendigital.denonline.in/Transaction/Home.aspx"
+    />
+    <Card
+      imgsrc="https://picsum.photos/id/1/200/300"
+      title="Dark"
+      sname="dark"
+      link="https://dendigital.denonline.in/Transaction/Home.aspx"
+    />
+    <Card
+      imgsrc="https://picsum.photos/id/1/200/300"
+      title="Umbrella"
+      sname="umbrella"
+      link="https://dendigital.denonline.in/Transaction/Home.aspx"
+    />
+  </>,
 
   document.getElementById("root")
 );
